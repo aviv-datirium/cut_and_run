62dcd87d97a037116493bade8637abb57a89b017
aviv-datirium <aviv.de.morgan@datirium.com>
2025-06-06 20:56:47 -0400

    bedgraphtobigwig bug fix

--- cut_and_run_pipeline.sh
+++ cut_and_run_pipeline.sh
@@ -191,14 +191,14 @@ do
     base_name=$(basename $dedup_bam .dedup.bam)
 
     # Step 5.1: Generate BedGraph from BAM using bedtools genomecov
-    bedtools genomecov -ibam $dedup_bam -bg > $OUTPUT_DIR/bigwig_bedgraphs/$base_name.bedgraph
+    bedtools genomecov -ibam $dedup_bam  -bg > $OUTPUT_DIR/bigwig_bedgraphs/$base_name.bedgraph
     if [ $? -ne 0 ]; then
         echo "BedGraph generation failed for $base_name. Check $LOG_DIR/bedgraph_error.log for details." | tee -a $LOG_DIR/pipeline.log
         exit 1
     fi
 
     # Step 5.2: Convert BedGraph to BigWig using bedGraphToBigWig (UCSC tool)
-    bedGraphToBigWig $OUTPUT_DIR/bigwig_bedgraphs/$base_name.bedgraph $GENOME_SIZE $OUTPUT_DIR/bigwig_bedgraphs/$base_name.bw
+    bedGraphToBigWig $OUTPUT_DIR/bigwig_bedgraphs/$base_name.bedgraph $OUTPUT_DIR/bigwig_bedgraphs/$base_name.bw
     if [ $? -ne 0 ]; then
         echo "BigWig conversion failed for $base_name. Check $LOG_DIR/bigwig_error.log for details." | tee -a $LOG_DIR/pipeline.log
         exit 1
