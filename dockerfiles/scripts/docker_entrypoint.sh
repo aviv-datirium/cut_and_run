c52033e407f44b61f62a7068e82f3c572c86d0d8
aviv-datirium <aviv.de.morgan@datirium.com>
2025-07-01 08:35:08 -0400

    test cwl of macs2

--- dockerfiles/scripts/docker_entrypoint.sh
+++ dockerfiles/scripts/docker_entrypoint.sh
@@ -3,18 +3,4 @@ set -e
 
 export PATH="/opt/conda/envs/cutrun/bin:$PATH"
 
-# Dynamically determine the current working directory inside the container
-TARGET_DIR=${TARGET_DIR:-$(pwd)}
-
-uid=$(stat -c %u "$TARGET_DIR")
-gid=$(stat -c %g "$TARGET_DIR")
-
-# Create matching group & user only if needed
-if ! getent group "$gid" >/dev/null; then
-  groupadd -g "$gid" hostgrp
-fi
-if ! id -u "$uid" >/dev/null 2>&1; then
-  useradd -m -u "$uid" -g "$gid" hostusr
-fi
-
-exec gosu "$uid:$gid" micromamba run -n cutrun /usr/local/bin/cutrun.sh "$@"
+/usr/local/bin/cutrun.sh "$@"
